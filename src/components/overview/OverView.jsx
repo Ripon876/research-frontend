20a93689390d3934b915dea6888ed294cd8c1c28
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-02 01:42:49 +0600

    overview added

---
+++ src/components/overview/OverView.jsx
@@ -0,0 +1,45 @@
+import './OverView.css';
+
+function OverView() {
+	return (
+		<>
+		<section id="why-us" className="why-us">
+		      <div className="container">
+
+		        <div className="row">
+		          
+		          <div className="col-lg-12 d-flex align-items-stretch">
+		            <div className="icon-boxes d-flex flex-column justify-content-center">
+		              <div className="row">
+		                <div className="col-xl-4 d-flex align-items-stretch" id="pub1">
+		                  <div className="icon-box mt-4 mt-xl-0">
+		                    <span>75+</span>
+		                    <h4>PUBLISHED REQUEST IN PROGRESS</h4>
+		                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
+		                  </div>
+		                </div>
+		                <div className="col-xl-4 d-flex align-items-stretch" id="pub2">
+		                  <div className="icon-box mt-4 mt-xl-0">
+		                    <span>130+</span>
+		                    <h4>PUBLISHED</h4>
+		                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
+		                  </div>
+		                </div>
+		                <div className="col-xl-4 d-flex align-items-stretch" id="pub3">
+		                  <div className="icon-box mt-4 mt-xl-0">
+		                    <span>45+</span>
+		                    <h4>REQUEST FOR PUBLISH</h4>
+		                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
+		                  </div>
+		                </div>
+		              </div>
+		            </div>
+		          </div>
+		        </div>
+		    </div>
+		</section>
+		</>
+	)
+}
+
+export default OverView;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Header from  '../components/header/Header';
 import Slider from  '../components/slider/Slider';
+import OverView from  '../components/overview/OverView';
 
 function Home() {
 	return (
 		<>
 			<Header />
 			<Slider />
+			<OverView />
 		</>
 	)
 }
