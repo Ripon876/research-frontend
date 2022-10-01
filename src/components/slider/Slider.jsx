e7199df999618b4c4c3fa95a9dd1f085118144e6
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-02 01:29:19 +0600

    slider updated

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -3,25 +3,25 @@ function Slider() {
 	return (
 		<>
 		  <section id="hero" >
-		      <div id="carouselExampleControls" class="carousel slide h-100" data-ride="carousel">
-		          <div class="carousel-inner  h-100">
-		            <div class="carousel-item active  h-100">
-		              <img class="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="First slide" />
+		      <div id="carouselExampleControls" className="carousel slide h-100" data-ride="carousel">
+		          <div className="carousel-inner  h-100">
+		            <div className="carousel-item active  h-100">
+		              <img className="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="First slide" />
 		            </div>
-		            <div class="carousel-item  h-100">
-		              <img class="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="Second slide" />
+		            <div className="carousel-item  h-100">
+		              <img className="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="Second slide" />
 		            </div>
-		            <div class="carousel-item  h-100">
-		              <img class="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="Third slide" />
+		            <div className="carousel-item  h-100">
+		              <img className="d-block w-100  h-100" src="assets/img/hero-bg.jpg" alt="Third slide" />
 		            </div>
 		          </div>
-		          <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-		            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-		            <span class="sr-only">Previous</span>
+		          <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+		            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+		            <span className="sr-only">Previous</span>
 		          </a>
-		          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-		            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-		            <span class="sr-only">Next</span>
+		          <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+		            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+		            <span className="sr-only">Next</span>
 		          </a>
 		        </div>
 		  </section>
