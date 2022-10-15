395b44f2a06f27653c3b2aaa042cbf0642a1e874
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-16 01:15:05 +0600

    sample data of research requests

---
+++ src/components/dashboard/moderator/research-requests/smd.js
@@ -0,0 +1,38 @@
+export const requests = [
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "43543hsdf3dsf4534s3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "43543hsdf34543sfs3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "43345543hsdf3dsfs3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "43543hsdf33dsfs3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "43543h4534sdf3dsfs3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+	{
+		researcher: "Jhon Doe",
+		topic: "sdf sdf sdf sd",
+		id: "434354hsdf3dsfs3d",
+		date: "Sat, 15 Oct 2022 10:15:18 GMT",
+	},
+];
