a8d36fb4c1f9e08f1bae4d94cce9d420a74d3afd
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-16 01:15:48 +0600

    sample data added with table

--- src/components/dashboard/moderator/research-requests/Requests.jsx
+++ src/components/dashboard/moderator/research-requests/Requests.jsx
@@ -1,13 +1,40 @@
-import React from 'react'
+import { Link } from "react-router-dom";
+import { requests } from "./smd";
 
 function Requests() {
-	return (
-		<div  className="container">
-			<div className="section-title">
-		        <h2>Requests</h2>
-		    </div>
-		</div>
-	)
+  return (
+    <div className="container">
+      <div className="section-title">
+        <h2>Requests</h2>
+
+        <table class="table mt-5">
+          <caption>All request submited by researchers</caption>
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">Researcher</th>
+              <th scope="col">Topic</th>
+              <th scope="col">Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {requests?.map((request, i) => (
+              <tr>
+                <th scope="row">{i + 1}</th>
+                <td>{request.researcher}</td>
+                <td>{request.topic}</td>
+                <td>
+                  <Link to={`/dashboard/moderator/requests/${request.id}`}>
+                    View
+                  </Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Requests;
