74c614ca2158f6b2c6fe6d682a544537362a129d
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-16 01:14:01 +0600

    moderator dashboard added

---
+++ src/components/dashboard/moderator/dashboard/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard() {
+	return (
+		<div className="container">
+			<div className="section-title">
+				<h2>Dashboard</h2>
+			</div>
+		</div>
+	);
+}
+
+export default Dashboard;
