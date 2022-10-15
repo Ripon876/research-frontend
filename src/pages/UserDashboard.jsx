8915dee235805e3ed96bbccbfbc10f9a5f6d9c4e
MD Ripon Islam <islam876ripon@gmail.com>
2022-10-16 01:25:18 +0600

    routes for pages updated

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,3 +1,5 @@
+import Profile from '../components/dashboard/user/profile/Profile';
+import Settings from '../components/dashboard/user/settings/Settings';
 import PublishedResearch from '../components/dashboard/user/published-research/PublishedResearch';
 import RequestForPublish from '../components/dashboard/user/request-for-publish/RequestForPublish';
 import ViewResearch from '../components/dashboard/user/view-research/ViewResearch';
@@ -15,12 +17,14 @@ function UserDashboard(){
         <div>
 			<Header userType='user' />
 			<section className="dashboard mt-4"  id="dashboard">
-		        <div className="row">
+		        <div className="row"  style={{background : '#0e141e'}}>
 		            <SideBar userType='user' />
-		            <div className="col-md-9 py-5">
+		            <div className="col-md-9 py-5  main-panel">
 						{page == 'published' && <PublishedResearch />}
 						{page == 'reqeust-for-publish' && <RequestForPublish />}
 						{page == 'view-research' && <ViewResearch />}
+						{page == 'profile' && <Profile />}
+						{page == 'settings' && <Settings />}
 		            </div>
 		        </div>
 			</section>
