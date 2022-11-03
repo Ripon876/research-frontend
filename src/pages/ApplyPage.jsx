57af122e85ed26bc515229789866ce9e5366cee2
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-03 17:59:24 +0600

    apply page added

---
+++ src/pages/ApplyPage.jsx
@@ -0,0 +1,15 @@
+import Internship from '../components/apply/internship/Internship';
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+
+function ApplyPage() {
+	return (
+		<div>
+			<Header />
+			<Internship />
+			<Footer />
+		</div>
+	)
+}
+
+export default ApplyPage;
