0f533fb35efb3858764a65040c21871e23caf043
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-03 18:37:17 +0600

    work page added

--- src/pages/ApplyPage.jsx
+++ src/pages/ApplyPage.jsx
@@ -1,15 +1,36 @@
 import Internship from '../components/apply/internship/Internship';
+import Work from '../components/apply/work/Work';
 import Header from  '../components/header/Header';
 import Footer from  '../components/footer/Footer';
 
-function ApplyPage() {
+function InternshipPage() {
 	return (
 		<div>
 			<Header />
+			<div className='mt-5 pt-5'></div>
+			<div className='mt-3 pt-3'></div>
 			<Internship />
+			<div className='mt-5 pt-5'></div>
 			<Footer />
 		</div>
 	)
 }
 
-export default ApplyPage;
+function WorkPage() {
+	return (
+		<div>
+			<Header />
+			<div className='mt-5 pt-5'></div>
+			<div className='mt-3 pt-3'></div>
+			<Work />
+			<div className='mt-5 pt-5'></div>
+			<Footer />
+		</div>
+	)
+}
+
+
+export {
+	InternshipPage,
+	WorkPage
+};
