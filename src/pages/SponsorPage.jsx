d5580d260b368bc2f7ecbc0701eec4f32f2859a6
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-04 02:48:46 +0600

    sponsor page added

---
+++ src/pages/SponsorPage.jsx
@@ -0,0 +1,15 @@
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+import Sponsor from '../components/sponsor_our_programs/Sponsor';
+
+function SponsorPage() {
+	return (
+		<div>
+		<Header />
+		<Sponsor />
+		<Footer />
+		</div>
+	)
+}
+
+export default SponsorPage;
