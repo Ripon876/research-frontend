82ad2bb5bc67a3a0a93a90922a72fccedbc87fa0
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-04 17:00:46 +0600

    donate page added

---
+++ src/pages/DonateUsPage.jsx
@@ -0,0 +1,16 @@
+import Header from  '../components/header/Header';
+import DonateUs from  '../components/donate_us/DonateUs';
+import Footer from  '../components/footer/Footer';
+
+
+function DonateUsPage() {
+	return (
+		<div>
+			<Header />
+			<DonateUs />
+			<Footer />
+		</div>
+	)
+}
+
+export default DonateUsPage;
