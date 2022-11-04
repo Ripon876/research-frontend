4af3e161fe875a8433859da3f1eedb082145a0c6
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-04 20:39:59 +0600

    challenges page added

---
+++ src/pages/ChallengesPage.jsx
@@ -0,0 +1,15 @@
+import Challenges from '../components/challenges/Challenges';
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+
+function ChallengesPage() {
+	return (
+		<div>
+			<Header />
+			<Challenges />
+			<Footer />
+		</div>
+	)
+}
+
+export default ChallengesPage;
