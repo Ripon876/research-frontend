d2b829051fb7740bcf095b66604005c4f115f263
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-06 17:30:42 +0600

    home_ 2 page added

---
+++ src/pages/Home_2.jsx
@@ -0,0 +1,15 @@
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+import Home from  '../components/home_2/Home';
+
+function Home_2() {
+	return (
+		<div>
+			<Header />
+			<Home />
+			<Footer />
+		</div>
+	)
+}
+
+export default Home_2;
