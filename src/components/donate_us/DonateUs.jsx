b9a22213b8b822e34c5300a6594007a85107fe72
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-07 22:03:30 +0600

    payment gateway added

--- src/components/donate_us/DonateUs.jsx
+++ src/components/donate_us/DonateUs.jsx
@@ -1,4 +1,4 @@
-import Donor from "./Donor";
+import Donors from "./Donors";
 
 function DonateUs() {
 	return (
@@ -9,10 +9,9 @@ function DonateUs() {
 						<div className="section-title mb-5 px-5">
 							<h2>Donate Us</h2>
 							<p>
-								Magnam dolores commodi suscipit. Necessitatibus
-								eius consequatur ex aliquid fuga eum quidem. Sit
-								sint consectetur velit. Quisquam quos quisquam
-								cupiditate.
+								Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+								ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+								quos quisquam cupiditate.
 							</p>
 						</div>
 					</div>
@@ -21,35 +20,56 @@ function DonateUs() {
 			<div className="container mb-5">
 				<div className="row">
 					<div className="col-md-6 mb-4">
-						<img
-							src="./assets/img/donate-us/gift.svg"
-							className="img-fluid"
-						/>
+						<img src="./assets/img/donate-us/gift.svg" className="img-fluid" />
 					</div>
 					<div className="col-md-6 d-flex align-items-center">
 						<div>
 							<p style={{ fontSize: "18px" }}>
-								Magnam dolores commodi suscipit. Necessitatibus
-								eius consequatur ex aliquid fuga eum quidem. Sit
-								sint consectetur velit. Quisquam quos quisquam
-								cupiditate.Magnam dolores commodi suscipit.
-								Necessitatibus eius consequatur ex aliquid fuga
-								eum quidem. Sit sint consectetur velit. Quisquam
-								quos quisquam cupiditate.
+								Magnam dolores commodi suscipit. Necessitatibus eius consequatur
+								ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
+								quos quisquam cupiditate.Magnam dolores commodi suscipit.
+								Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit
+								sint consectetur velit. Quisquam quos quisquam cupiditate.
 							</p>
 							<button className="btn subBtn">Donate Now</button>
 						</div>
 					</div>
 				</div>
 			</div>
-			<div className="container mb-5">
-				<h4 className="font-weight-bold my-4">Our Donors</h4>
-				<div className="justify-content-around row">
-					<Donor />
-					<Donor />
-					<Donor />
-					<Donor />
-					<Donor />
+			<Donors />
+			<div className="container my-5">
+				<h4 className="font-weight-bold my-4">Donate us via</h4>
+				<div>
+					<div className='d-flex justify-content-around'>
+						<div style={{ height: "100px", width: "200px" ,cursor: 'pointer'}} className='rounded shadow p-2'>
+							<img
+								src="https://freelogopng.com/images/all_img/1656227518bkash-logo-png.png"
+								className="img-fluid rounded w-100 h-100"
+								style={{ objectFit: "contain" }}
+							/>
+						</div>
+						<div style={{ height: "100px", width: "200px" ,cursor: 'pointer'}} className='rounded shadow p-2'>
+							<img
+								src="https://www.freepnglogos.com/uploads/visa-card-logo-9.png"
+								className="img-fluid rounded w-100 h-100"
+								style={{ objectFit: "contain" }}
+							/>
+						</div>
+						<div style={{ height: "100px", width: "200px" ,cursor: 'pointer'}} className='rounded shadow p-2'>
+							<img
+								src="https://www.freepnglogos.com/uploads/discover-png-logo/mastercard-discover-logo-png-22.png"
+								className="img-fluid rounded w-100 h-100"
+								style={{ objectFit: "contain" }}
+							/>
+						</div>
+						<div style={{ height: "100px", width: "200px" ,cursor: 'pointer'}} className='rounded shadow p-2'>
+							<img
+								src="https://seeklogo.com/images/D/dutch-bangla-rocket-logo-B4D1CC458D-seeklogo.com.png"
+								className="img-fluid rounded w-100 h-100"
+								style={{ objectFit: "contain" }}
+							/>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
