4842803db8b6875e1270ca00674d720839b58a94
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-07 20:18:27 +0600

    profile pic added in comments

--- src/components/home_2/posts/comments/Comment.jsx
+++ src/components/home_2/posts/comments/Comment.jsx
@@ -5,11 +5,19 @@ function Comment() {
 		<div className="border-0 card">
 			<div className="card-body">
 				<div className="d-flex justify-content-between">
-					<div>
-						<h5 className="card-title mb-1">Mandela</h5>
-						<h6 className="card-subtitle mb-2 text-muted">
-							Civil Engineer
-						</h6>
+					<div className="align-items-center d-flex">
+						<div className="mr-3">
+							<img
+								src="https://via.placeholder.com/40x40"
+								className="img-fluid rounded-circle"
+							/>
+						</div>
+						<div className='pt-1'>
+							<h5 className="card-title mb-1">Jhon Doe</h5>
+							<h6 className="card-subtitle mb-2 text-muted">
+								Civil Engineer
+							</h6>
+						</div>
 					</div>
 					<div className="p-1">
 						<h6 className="card-subtitle mb-2 text-muted">
