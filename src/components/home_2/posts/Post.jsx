2fbe2f9b6938e76fbf6ae5bab6f734db81d2074c
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-07 20:11:41 +0600

    profile picture added on post

--- src/components/home_2/posts/Post.jsx
+++ src/components/home_2/posts/Post.jsx
@@ -2,22 +2,34 @@ import { Fade } from "react-reveal";
 import Moment from "react-moment";
 import Comments from "./comments/Comments";
 
-function Post({i}) {
+function Post({ i }) {
 	return (
 		<Fade>
 			<div className="container my-5 rounded shadow p-3">
 				<div className="row">
-					<div className={`col-md-7 mb-4 ${i % 2 === 0 ? 'order-md-1' : ''}`}>
+					<div
+						className={`col-md-7 mb-4 ${
+							i % 2 === 0 ? "order-md-1" : ""
+						}`}
+					>
 						<div className="card">
 							<div className="card-body">
-								<div className="d-flex justify-content-between">
-									<div>
-										<h4 className="card-title mb-1">
-											Jhon Doe
-										</h4>
-										<h6 className="card-subtitle mb-2 text-muted">
-											Civil Engineer
-										</h6>
+								<div className="d-flex justify-content-between mb-2">
+									<div className="align-items-center d-flex">
+										<div className="mr-3">
+											<img
+												src="https://via.placeholder.com/50x50"
+												className="img-fluid rounded-circle"
+											/>
+										</div>
+										<div>
+											<h4 className="card-title mb-1">
+												Jhon Doe
+											</h4>
+											<h6 className="card-subtitle mb-2 text-muted">
+												Civil Engineer
+											</h6>
+										</div>
 									</div>
 									<div className="p-1">
 										<h6 className="card-subtitle mb-2 text-muted">
@@ -52,7 +64,7 @@ function Post({i}) {
 						</div>
 					</div>
 					<div className="col-md-5 mt-4">
-					<h5 className="card-title mb-1">Comments (3)</h5>
+						<h5 className="card-title mb-1">Comments (3)</h5>
 						<Comments />
 					</div>
 				</div>
@@ -62,5 +74,3 @@ function Post({i}) {
 }
 
 export default Post;
-
-  
