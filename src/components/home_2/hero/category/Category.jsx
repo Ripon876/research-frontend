9acb7e681e6f774cf8522cee072afec1395e540d
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-07 17:15:19 +0600

    all list item bold

--- src/components/home_2/hero/category/Category.jsx
+++ src/components/home_2/hero/category/Category.jsx
@@ -9,21 +9,21 @@ function Category() {
 					<ul className="timeline">
 						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
 							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/be-a-researcher'>Request to be a researcher</Link></h2>
+							<h2 className="h5 mb-0"><Link to='/be-a-researcher'><b>Request to be a researcher</b></Link></h2>
 						</li>
 						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
 							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/research-my-problem'>Request for a solution</Link></h2>
+							<h2 className="h5 mb-0"><Link to='/research-my-problem'><b>Request for a solution</b></Link></h2>
 
 						</li>
 						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
 							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='publication-request'>Request to publish your work</Link></h2>
+							<h2 className="h5 mb-0"><Link to='publication-request'><b>Request to publish your work</b></Link></h2>
 
 						</li>
 						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
 							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/'>Research partner requirment</Link></h2>
+							<h2 className="h5 mb-0"><Link to='/'><b>Research partner requirment</b></Link></h2>
 						</li>
 						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
 							<div className="timeline-arrow"></div>
