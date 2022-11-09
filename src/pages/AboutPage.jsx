3f5931d0f99329e0bde574cd9d5a267171cd193b
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-09 20:43:52 +0600

    title added

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,31 +1,46 @@
-import Header from  '../components/header/Header';
-import Slider from  '../components/slider/Slider';
-import OverView from  '../components/overview/OverView';
-import About from  '../components/about/About';
-import Awards from  '../components/awards/Awards';
-import Publication from  '../components/publication/Publication';
-import Departments from  '../components/departments/Departments';
-import Sponsors from  '../components/sponsors/Sponsors';
-import FAQ from  '../components/faq/FAQ';
-import Contact from  '../components/contact/Contact';
-import Footer from  '../components/footer/Footer';
+import Header from "../components/header/Header";
+import Slider from "../components/slider/Slider";
+import OverView from "../components/overview/OverView";
+import About from "../components/about/About";
+import Awards from "../components/awards/Awards";
+import Publication from "../components/publication/Publication";
+import Departments from "../components/departments/Departments";
+import Sponsors from "../components/sponsors/Sponsors";
+import FAQ from "../components/faq/FAQ";
+import Contact from "../components/contact/Contact";
+import Footer from "../components/footer/Footer";
 
 function AboutPage() {
 	return (
 		<div>
 			<Header />
-			<Slider />
+			<div className="align-items-center d-flex hero justify-content-center mt-5 pt-5">
+				<div>
+					<div className="container mt-5 pt-5">
+						<div className="section-title mb-5 px-5">
+							<h2>About us</h2>
+							<p>
+								Magnam dolores commodi suscipit. Necessitatibus
+								eius consequatur ex aliquid fuga eum quidem. Sit
+								sint consectetur velit. Quisquam quos quisquam
+								cupiditate.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			{/*<Slider />*/}
 			<OverView />
 			<About />
 			<Awards />
-			<Publication />
+			{/*<Publication />*/}
 			<Departments />
 			<Sponsors />
 			<FAQ />
 			<Contact />
 			<Footer />
 		</div>
-	)
+	);
 }
 
 export default AboutPage;
