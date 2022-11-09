2dac6f6577c7035e02d61a4ffdb6bf84a02750bb
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-09 19:50:10 +0600

    about us page added

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,31 @@
+import Header from  '../components/header/Header';
+import Slider from  '../components/slider/Slider';
+import OverView from  '../components/overview/OverView';
+import About from  '../components/about/About';
+import Awards from  '../components/awards/Awards';
+import Publication from  '../components/publication/Publication';
+import Departments from  '../components/departments/Departments';
+import Sponsors from  '../components/sponsors/Sponsors';
+import FAQ from  '../components/faq/FAQ';
+import Contact from  '../components/contact/Contact';
+import Footer from  '../components/footer/Footer';
+
+function AboutPage() {
+	return (
+		<div>
+			<Header />
+			<Slider />
+			<OverView />
+			<About />
+			<Awards />
+			<Publication />
+			<Departments />
+			<Sponsors />
+			<FAQ />
+			<Contact />
+			<Footer />
+		</div>
+	)
+}
+
+export default AboutPage;
