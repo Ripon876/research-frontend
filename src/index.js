7ce7160d2f60628b5d63858fd5c1a4351c7a28a8
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-09 16:15:48 +0600

    automatically srolling to top after route change

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router} from "react-router-dom";
+import React from "react";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router> 
+    <Router>
+      <ScrollToTop />
       <App />
-    </Router> 
+    </Router>
   </React.StrictMode>
 );
+function ScrollToTop() {
+  const { pathname } = useLocation();
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
