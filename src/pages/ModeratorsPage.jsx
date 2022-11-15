a64503d38973aeb8d9bc46b4722664f549e7916e
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-15 22:11:53 +0600

    separete moderator page added

---
+++ src/pages/ModeratorsPage.jsx
@@ -0,0 +1,16 @@
+import Moderators from '../components/moderators/Moderators';
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+
+
+function ModeratorsPage() {
+	return (
+		<div>
+			<Header />
+			<Moderators />
+			<Footer />
+		</div>
+	)
+}
+
+export default ModeratorsPage;
