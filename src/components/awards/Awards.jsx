c65edccc77fc2815bbb8f45d4cfa0345206236b2
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-16 15:32:42 +0600

    title changed

--- src/components/awards/Awards.jsx
+++ src/components/awards/Awards.jsx
@@ -8,7 +8,7 @@ function Awards() {
 	    <section id="counts" className="counts">
 	      <div className="container">
           <div className="section-title mb-5">
-            <h2>We</h2>
+            <h2>Awards</h2>
             <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
           </div>
 	        <div className="row">
