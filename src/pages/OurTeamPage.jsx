74b6d1bb7e3693c7f64601cee69b725ed611280a
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-16 17:14:12 +0600

    our team page added

---
+++ src/pages/OurTeamPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import OurTeam from "../components/ourTeam/OurTeam";
+
+function OurTeamPage() {
+	return (
+		<div>
+			<Header />
+			<OurTeam />
+			<Footer />
+		</div>
+	)
+}
+
+export default OurTeamPage;
