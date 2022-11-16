279d713d02dadff93377a8b30e5b9e50c561ace4
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-16 17:29:03 +0600

    our vision page created

---
+++ src/pages/OurVisionPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import OurVision from '../components/ourVision/OurVision';
+
+function OurVisionPage() {
+	return (
+		<div>
+			<Header />
+			<OurVision />
+			<Footer />
+		</div>
+	)
+}
+
+export default OurVisionPage;
