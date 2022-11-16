a56c2a2e1896b18070855dcac8d4b6ff806277e3
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-16 17:22:32 +0600

    route added for our team

--- src/components/awards/smd.js
+++ src/components/awards/smd.js
@@ -2,7 +2,8 @@ export const cardData = [
 	{
 		icon: 'icofont-doctor-alt',
 		title: 'Our Team',
-		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod'
+		description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod',
+		route: '/our_team'
 	},{
 		icon: 'icofont-patient-bed',
 		title: 'Our Vision',
