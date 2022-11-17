9015e73f49312f1349a1bdd9aa48ece0a532ebde
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-17 16:20:07 +0600

    awards section replaced

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import Header from  '../components/header/Header';
 import Slider from  '../components/slider/Slider';
 import OverView from  '../components/overview/OverView';
 import About from  '../components/about/About';
-import Awards from  '../components/awards/Awards';
+import We from  '../components/we/We';
 import Publication from  '../components/publication/Publication';
 import Departments from  '../components/departments/Departments';
 import Sponsors from  '../components/sponsors/Sponsors';
@@ -17,7 +17,7 @@ function Home() {
 			<Slider />
 			<OverView />
 			<About />
-			<Awards />
+			<We />
 			<Publication />
 			<Departments />
 			<Sponsors />
