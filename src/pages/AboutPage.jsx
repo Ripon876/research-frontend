27357cdfad465970d8f33cc617df274501c0f94e
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-17 16:19:02 +0600

    awards section replaced

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ import Header from "../components/header/Header";
 import Slider from "../components/slider/Slider";
 import OverView from "../components/overview/OverView";
 import About from "../components/about/About";
-import Awards from "../components/awards/Awards";
+import We from "../components/we/We";
 import Publication from "../components/publication/Publication";
 import Departments from "../components/departments/Departments";
 import Moderators from "../components/about/moderators/Moderators";
@@ -32,10 +32,10 @@ function AboutPage() {
 				</div>
 			</div>
 			<OverView />
-			<Awards />
+			<We />
 			<Moderators />
-			<Sponsors />
 			<Gallery />
+			<Sponsors />
 			<Departments />
 			<FAQ />
 			<Contact />
