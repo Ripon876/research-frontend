fa6f846785df0f5059913ecbbd5e3716816c7309
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-17 15:57:19 +0600

    research labs page created

---
+++ src/pages/ResearchLabsPage.jsx
@@ -0,0 +1,15 @@
+import Header from "../components/header/Header";
+import Footer from "../components/footer/Footer";
+import ResearchLabs from '../components/researchLabs/ResearchLabs';
+
+function ResearchLabsPage() {
+	return (
+		<div>
+			<Header />
+			<ResearchLabs />
+			<Footer />
+		</div>
+	)
+}
+
+export default ResearchLabsPage;
