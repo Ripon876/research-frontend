004f242b19a7a76de71cf738e80de43f223b9ce1
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-20 00:35:10 +0600

    old form replaced

--- src/components/research-my-problem/ResearchMyProblem.jsx
+++ src/components/research-my-problem/ResearchMyProblem.jsx
@@ -1,49 +1,117 @@
-import './ResearchMyProblem.css';
+import { useEffect, useState } from "react";
+import "./ResearchMyProblem.css";
 
 function ResearchMyProblem() {
-	return (
-		<>
-	    <section className="inner-page">
-	      <div className="container">
-        <div className="row">
-          <div className="col-md-6 align-items-center col-md-6 d-flex">
-            <img src="assets/img/about.jpg" className="img-fluid rounded" />
+  const initialData = {
+    name: "",
+    email: "",
+    title: "",
+    topic: "",
+    reason: "",
+    description: "",
+  };
+
+  const [formData, updateFormData] = useState(initialData);
+
+  const handleChange = (event) => {
+    if (event === null) {
+      updateFormData({
+        ...formData,
+        work_as: "",
+      });
+      return;
+    }
+    if (event.value) {
+      updateFormData({
+        ...formData,
+        work_as: event.value,
+      });
+    } else {
+      updateFormData({
+        ...formData,
+        [event.target.name]: event.target.value.trim(),
+      });
+    }
+  };
+
+  const handleSubmit = () => {
+    console.log(formData);
+  };
+
+  return (
+    <div>
+      <div className="registration-form my-5">
+        <form style={{ maxWidth: "650px" }}>
+          <div className="d-flex justify-content-center align-items-baseline mb-4">
+            <h4>Request To Research Your Problem</h4>
           </div>
-          <div className="col-md-6 pt-5">
-            <div className="box mx-2">
-              <form>
-              <h4 className="font-weight-bold">Request To Research Your Problem</h4>
-              <div className="form-group">                
-                <input type="text" name="name" className="form-control" placeholder="Fullname" />
-              </div>
-              <div className="form-group">             
-                <input type="email" name="Email" className="form-control" placeholder="Email" />
-              </div>
-              <div className="form-group">   
-                <input type="text" name="title" className="form-control" placeholder="Title of Publication Article" />
-              </div>
-              <div className="form-group">
-                <input type="text" name="title" className="form-control" placeholder="Topic" />
-              </div>
-              <div className="form-group">
-                <label>Why do you want to research on this topic?</label>
-                <input type="text" name="title" className="form-control" />
-              </div>
-              <div className="form-group">
-                <label>Description</label>
-                <textarea className="form-control" placeholder="Description" rows='6'></textarea>
-              </div>
-              <div className="form-group mt-4 text-center">
-                <input type="submit" name="submit" className="btn btn-primary btn-sm px-5 py-2 rounded-pill subBtn" />
-              </div>
-            </form>
-            </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              name="name"
+              placeholder="Full name"
+              onChange={handleChange}
+            />
           </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              name="email"
+              placeholder="Email"
+              onChange={handleChange}
+            />
           </div>
-        </div>
-      </section>
-		</>
-	)
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              name="title"
+              placeholder="Title of Publication Article"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              name="topic"
+              placeholder="Topic"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <input
+              type="text"
+              className="form-control item"
+              name="reason"
+              placeholder="Why do you want to research on this topic?"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              placeholder="Description"
+              rows="7"
+              name="description"
+              onChange={handleChange}
+            ></textarea>
+          </div>
+          <div className="form-group">
+            <button
+              type="button"
+              className="btn btn-block submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default ResearchMyProblem;
