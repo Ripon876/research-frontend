9c2ce00353b5aa3f4f4ccc931f356b4d5e94cb83
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-19 15:39:59 +0600

    commment removed

--- src/components/institutions/Institutions.jsx
+++ src/components/institutions/Institutions.jsx
@@ -1,20 +1,21 @@
-import Card from './Card';
-import './Institutions.css';
-import {institutions} from './smd';
+import Card from "./Card";
+import "./Institutions.css";
+import { institutions } from "./smd";
 
 function Institutions() {
-	return (
+  return (
     <section id="counts" className="counts pt-0">
       <div className="container mt-4">
         <h4 className="font-weight-bold my-4">Best Research institution</h4>
         <div className="row">
-          {institutions?.map((item) => 
+          {/*{institutions?.map((item) => 
             <Card item={item} />
-          )}
+          )}*/}
+          
         </div>
       </div>
     </section>
-	)
+  );
 }
 
 export default Institutions;
