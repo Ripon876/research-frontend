eb93e75cf42f21edf7c066d3dfdd67a9010a5be7
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-19 16:00:22 +0600

    dynamically adding list items

--- src/components/home_2/hero/category/Category.jsx
+++ src/components/home_2/hero/category/Category.jsx
@@ -1,4 +1,5 @@
-import {Link} from 'react-router-dom';
+import { Link } from "react-router-dom";
+import {list} from './smd';
 import "./Category.css";
 
 function Category() {
@@ -7,32 +8,16 @@ function Category() {
 			<div className="row">
 				<div className="col-lg-7 mx-auto">
 					<ul className="timeline">
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/be-a-researcher'><b>Request to be a researcher</b></Link></h2>
-						</li>
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/research-my-problem'><b>Request for a solution</b></Link></h2>
-
-						</li>
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='publication-request'><b>Request to publish your work</b></Link></h2>
-
-						</li>
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/'><b>Research partner requirment</b></Link></h2>
-						</li>
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/apply/internship'><b>Apply for internship</b></Link></h2>
-						</li>
-						<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
-							<div className="timeline-arrow"></div>
-							<h2 className="h5 mb-0"><Link to='/apply/work'><b>Apply for Work</b></Link></h2>
-						</li>
+						{list?.map((item) => (
+							<li className="timeline-item bg-white rounded ml-3 p-4 shadow">
+								<div className="timeline-arrow"></div>
+								<h2 className="h5 mb-0">
+									<Link to={item?.route}>
+										<b>{item?.title}</b>
+									</Link>
+								</h2>
+							</li>
+						))}
 					</ul>
 				</div>
 			</div>
