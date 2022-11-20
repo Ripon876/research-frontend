4d09b52087f992569dc3903d309c2ebd943d9e15
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-20 23:27:37 +0600

    top challengers page craeted

---
+++ src/pages/TopChallengersPage.jsx
@@ -0,0 +1,16 @@
+import Header from  '../components/header/Header';
+import Footer from  '../components/footer/Footer';
+import TopChallengers from  '../components/topChallengers/TopChallengers';
+
+
+function TopChallengersPage() {
+	return (
+		<div>
+			<Header />
+			<TopChallengers />
+			<Footer />
+		</div>
+	)
+}
+
+export default TopChallengersPage;
