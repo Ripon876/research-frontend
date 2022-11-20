cda1c60914e2a98ed088c996a4c70113bf3ffe5c
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-20 15:45:06 +0600

    data structure updated

--- src/components/awards/awardTable/smd.js
+++ src/components/awards/awardTable/smd.js
@@ -1,198 +1,148 @@
 export const data = [
 	{
-		serial: 1,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2022",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 2,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2020",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 3,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2021",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 4,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2022",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 5,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2018",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 6,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2019",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 7,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2018",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 8,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 9,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 10,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 11,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2022",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 12,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2020",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 13,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2021",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 14,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2022",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 15,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2018",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 16,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2019",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 17,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2018",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 18,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 19,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-	{
-		serial: 20,
-		name: "Lorem ipsum dolor sit amet",
-		year: "2017",
-		award: "Dolor sit amet",
-	},
-];
-
-const newD = [
-	{
 		year: 2022,
 		best_researchers: [
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 1
 			},
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 2
 			},
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 3
 			},
 		],
 		best_institutions: [
 			{
-				name: "University",
-				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 1
 			},
 			{
-				name: "University",
-				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 2
 			},
 			{
-				name: "University",
-				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 3
 			},
 		],
 	},
 	{
-		year: 2022,
+		year: 2021,
 		best_researchers: [
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 1
 			},
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 2
 			},
 			{
 				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 3
 			},
 		],
 		best_institutions: [
 			{
-				name: "University",
-				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 1
 			},
 			{
-				name: "University",
-				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 2
 			},
 			{
-				name: "University",
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 3
+			},
+		],
+	},
+	{
+		year: 2020,
+		best_researchers: [
+			{
+				name: "Jhone Doe",
 				institution: "Lorem Ipsum Dollar Sit",
-				img: "https://via.placeholder.com/200x200",
+				position: 1
+			},
+			{
+				name: "Jhone Doe",
+				institution: "Lorem Ipsum Dollar Sit",
+				position: 2
+			},
+			{
+				name: "Jhone Doe",
+				institution: "Lorem Ipsum Dollar Sit",
+				position: 3
+			},
+		],
+		best_institutions: [
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 1
+			},
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 2
+			},
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 3
+			},
+		],
+	},{
+		year: 2019,
+		best_researchers: [
+			{
+				name: "Jhone Doe",
+				institution: "Lorem Ipsum Dollar Sit",
+				position: 1
+			},
+			{
+				name: "Jhone Doe",
+				institution: "Lorem Ipsum Dollar Sit",
+				position: 2
+			},
+			{
+				name: "Jhone Doe",
+				institution: "Lorem Ipsum Dollar Sit",
+				position: 3
+			},
+		],
+		best_institutions: [
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 1
+			},
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 2
+			},
+			{
+				name: "University desazsdsada",
+				address: "Lorem Ipsum Dollar Sit",
+				position: 3
 			},
 		],
 	},
