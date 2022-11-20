5081c20e282698854048cea9d9c1137fc16838ec
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-20 15:46:46 +0600

    table row added & showing table dynamically

--- src/components/awards/awardTable/AwardTable.jsx
+++ src/components/awards/awardTable/AwardTable.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import TableRow from './TableRow';
 import { data } from "./smd";
+
 
 function AwardTable() {
 	const [tableDate, setTableData] = useState(data);
@@ -36,105 +38,6 @@ function AwardTable() {
 					<i className="icofont-filter filterButton"></i>
 				</h2>
 			</div>
-			{/*<table
-				className="table table-bordered text-center"
-				style={{ fontSize: "18px" }}
-			>
-				<thead>
-					<tr>
-						<th scope="col">Year</th>
-						<th scope="col" colSpan="3">
-							Results
-						</th>
-					</tr>
-				</thead>
-				<tbody>
-					<tr>
-						<th scope="row">2022</th>
-						<td className="p-0">
-							<table class="table m-0">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Best Researchers</th>
-										<th scope="col">Award</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">1</th>
-										<td>Mark</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">2</th>
-										<td>Jacob</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th>
-										<td>Larry</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</td>
-						<td className="p-0">
-							<table class="table m-0">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Best Researchers</th>
-										<th scope="col">Award</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">1</th>
-										<td>Mark</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">2</th>
-										<td>Jacob</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th>
-										<td>Larry</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</td>
-					</tr>
-				</tbody>
-			</table>*/}
-
 			<table
 				className="table table-bordered text-center"
 				style={{ fontSize: "18px" }}
@@ -148,90 +51,11 @@ function AwardTable() {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<th scope="row">2022</th>
-						<td className="p-0">
-							<table class="table m-0">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Best Researchers</th>
-										<th scope="col">Award</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">1</th>
-										<td>Mark</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">2</th>
-										<td>Jacob</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th>
-										<td>Larry</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-							<table class="table m-0">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Best Institutions</th>
-										<th scope="col">Award</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<th scope="row">1</th>
-										<td>Mark</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">2</th>
-										<td>Jacob</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-									<tr>
-										<th scope="row">3</th>
-										<td>Larry</td>
-										<td>
-											<span className="text-secondary">
-												Award nane
-											</span>
-										</td>
-									</tr>
-								</tbody>
-							</table>
-						</td>
-					</tr>
+				{data?.map((row) =>
+					<TableRow data={row} />
+				)}
 				</tbody>
 			</table>
-
 			<nav>
 				<ul className="pagination text-dark">
 					<li className="page-item">
