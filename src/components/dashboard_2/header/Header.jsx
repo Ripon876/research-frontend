a297c395d75e57d8c2acc56303e701ecf70ecf75
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-22 02:46:36 +0600

    combined all dashboard components

--- src/components/dashboard_2/header/Header.jsx
+++ src/components/dashboard_2/header/Header.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import Info from "./profileInfo/Info";
+import Notifications from "./notifications/Notifications";
+import "./Header.css";
 
 function Header() {
 	return (
-		<div>
-			
+		<div className="dashboardHeader">
+			<div className="container-fluid" style={{ maxWidth: "1600px" }}>
+				<div className="row align-items-center justify-content-between">
+					<Info />
+					<Notifications />
+				</div>
+			</div>
 		</div>
-	)
+	);
 }
 
 export default Header;
