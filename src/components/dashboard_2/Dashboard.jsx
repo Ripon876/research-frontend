cc5d2cb7e6c59925e9e18f4c8221396eda13d231
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-22 00:17:18 +0600

    controlar component added

---
+++ src/components/dashboard_2/Dashboard.jsx
@@ -0,0 +1,18 @@
+import {useParams} from 'react-router-dom';
+import M_Dashboard from './moderator/Dashboard';
+import R_Dashboard from './researcher/Dashboard';
+
+function Dashboard() {
+	const {type} = useParams();
+	return (
+		<div>
+			{type === 'moderator ' ? 
+				<M_Dashboard />
+				: 
+				<R_Dashboard />
+		    }
+		</div>
+	)
+}
+
+export default Dashboard;
