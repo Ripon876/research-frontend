3e8e2f36b6a25c6150cba7a6d3566c24650d3370
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-21 23:59:02 +0600

    dashboard structure created

--- src/components/dashboard_2/researcher/Dashboard.jsx
+++ src/components/dashboard_2/researcher/Dashboard.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import Header from '../header/Header';
+import Posts from '../posts/Posts';
 
 function R_Dashboard() {
 	return (
 		<div>
-			
+			<Header />
+			<Posts />
 		</div>
-	)
+	);
 }
 
 export default R_Dashboard;
