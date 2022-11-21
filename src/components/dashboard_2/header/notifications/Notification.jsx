005f243bf5d1e4ba653f829e288c77219feea109
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-22 02:45:19 +0600

    notification separated

--- src/components/dashboard_2/header/notifications/Notification.jsx
+++ src/components/dashboard_2/header/notifications/Notification.jsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
 function Notification() {
 	return (
-		<div>
-			
+		<div className="toast mx-auto" style={{ opacity: 1 }}>
+			<div className="toast-header">
+				<img src="https://via.placeholder.com/20x20" class="img-fluid mr-2" />
+				<strong className="mr-auto">Mark</strong>
+				<small>11 mins ago</small>
+			</div>
+			<div className="toast-body">Hello, world! This is a Notification.</div>
 		</div>
-	)
+	);
 }
 
 export default Notification;
