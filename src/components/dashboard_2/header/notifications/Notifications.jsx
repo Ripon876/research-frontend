eabd4485349320137441b846689195ff82a2a8b3
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-22 02:46:03 +0600

    notification component created & added some styles

--- src/components/dashboard_2/header/notifications/Notifications.jsx
+++ src/components/dashboard_2/header/notifications/Notifications.jsx
@@ -1,11 +1,22 @@
-import Notification from './Notification'
+import Notification from "./Notification";
 
 function Notifications() {
 	return (
-		<div>
-			
+		<div className="col-md-4 py-4">
+			<div className="notifications">
+				<h4
+					className="mb-1 text-secondary"
+					style={{ paddingLeft: "33px" }}
+				>
+					Notifications
+				</h4>
+				<Notification />
+				<Notification />
+				<Notification />
+				<Notification />
+			</div>
 		</div>
-	)
+	);
 }
 
 export default Notifications;
