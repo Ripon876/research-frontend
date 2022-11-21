efa5d2bfe1f792357e9e82539e28b343b1742d02
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-21 23:57:41 +0600

    identical name given

--- src/components/dashboard_2/researcher/Dashboard.jsx
+++ src/components/dashboard_2/researcher/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Dashboard() {
+function R_Dashboard() {
 	return (
 		<div>
 			
@@ -8,4 +8,4 @@ function Dashboard() {
 	)
 }
 
-export default Dashboard
+export default R_Dashboard;
