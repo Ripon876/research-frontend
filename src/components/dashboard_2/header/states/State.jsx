e7c8659168f3275ea8e9966407fdf2e1e8e205ba
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 03:07:44 +0600

    file formated properly

--- src/components/dashboard_2/header/states/State.jsx
+++ src/components/dashboard_2/header/states/State.jsx
@@ -4,7 +4,7 @@ function State() {
 	return (
 		<div className="col-4 px-1">
 			<div
-				class="d-flex align-items-center m-2"
+				className="d-flex align-items-center m-2"
 				style={{
 					height: "110px",
 					// width: "10rem",
@@ -12,9 +12,9 @@ function State() {
 					boxShadow: "0 0.25rem 0.75rem rgb(0 0 0 / 10%)",
 				}}
 			>
-				<div class="card-body text-center p-2">
-					<h2 class="card-title mb-0">15</h2>
-					<h5 class="card-title">Card title</h5>
+				<div className="card-body text-center p-2">
+					<h2 className="card-title mb-0">15</h2>
+					<h5 className="card-title">Card title</h5>
 				</div>
 			</div>
 		</div>
