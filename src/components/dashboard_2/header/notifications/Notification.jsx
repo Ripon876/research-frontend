255397fada3462608835d169ba8794ac57d2f0eb
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 03:08:26 +0600

    class keyword replaced with className

--- src/components/dashboard_2/header/notifications/Notification.jsx
+++ src/components/dashboard_2/header/notifications/Notification.jsx
@@ -4,7 +4,7 @@ function Notification() {
 	return (
 		<div className="toast mx-auto" style={{ opacity: 1 }}>
 			<div className="toast-header">
-				<img src="https://via.placeholder.com/20x20" class="img-fluid mr-2" />
+				<img src="https://via.placeholder.com/20x20" className="img-fluid mr-2" />
 				<strong className="mr-auto">Mark</strong>
 				<small>11 mins ago</small>
 			</div>
