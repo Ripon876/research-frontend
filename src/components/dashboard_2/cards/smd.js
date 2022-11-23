1eb39acd8e635c883cfaecca00954da83b5f436e
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 02:57:49 +0600

    sample data for cards added

---
+++ src/components/dashboard_2/cards/smd.js
@@ -0,0 +1,117 @@
+export const postData = [
+	{
+		id: "43kjjs3dsfew",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-11-17",
+		status: "Review",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjswedsre",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-10-27",
+		status: "Review",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjdds4gs3",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-10-25",
+		status: "Approved",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjs936725",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-11-15",
+		status: "Review",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjs3kdr89",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-11-14",
+		status: "Denied",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjs3l9g5e",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-11-23",
+		status: "Review",
+		reviewMessage: "",
+	},
+	{
+		id: "43kjjs3p02v8",
+		title: "Lorem ipsum dolor sit amet, consectetur",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.",
+		date: "2022-11-07",
+		status: "Approved",
+		reviewMessage: "",
+	},
+];
+
+export const researchData = [
+	{
+		name: "Jhone Doe",
+		email: "jhonedoe@gmail.com",
+		title: "Air Pollution",
+		topic: "Air",
+		reason: "Lorem ipsum dolor sit amet",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqaliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliq",
+		date: "2022-11-22",
+		status: "Review",
+		reviewMessage: "",
+	},
+	{
+		name: "Jhone Doe",
+		email: "jhonedoe@gmail.com",
+		title: "Air Pollution",
+		topic: "Air",
+		reason: "Lorem ipsum dolor sit amet",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqaliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliq",
+		date: "2022-11-19",
+		status: "Approved",
+		reviewMessage: "",
+	},
+];
+
+export const articleData = [
+	{
+		name: "Jhone Doe",
+		email: "jhonedoe@gmail.com",
+		title: "Importance of Water ",
+		reason: "Lorem ipsum dolor sit amet",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqaliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliq",
+		date: "2022-11-21",
+		status: "Denied",
+		reviewMessage: "",
+	},
+	{
+		name: "Jhone Doe",
+		email: "jhonedoe@gmail.com",
+		title: "Understanding the Flow of Water ",
+		reason: "Lorem ipsum dolor sit amet",
+		description:
+			"Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqaliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliqMagnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sintconsectetur velit. Quisquam quos quisquam cupiditate.Magnam dolores commodi suscipit. Necessitatibus eiusconsequatur ex aliq",
+		date: "2022-11-21",
+		status: "Review",
+		reviewMessage: "",
+	},
+];
