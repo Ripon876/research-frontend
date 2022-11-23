13a0e9f27cf4f4cd865f4e60915eb4bbe9d894ec
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 03:03:33 +0600

    post & researcher modal components created

---
+++ src/components/dashboard_2/modal/Post.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Post({post}) {
+	return (
+		<div className='pt-3' style={{overflowY: 'scroll',height: '85%',boxShadow: 'inset -20px 0rem 1rem rgb(0 0 0 / 15%) !important'}}>
+			<h5 className="mb-0">{post?.title}</h5>
+			<div className='py-3'>
+				<div>
+					<p>
+						{post?.description}
+					</p>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Post;
