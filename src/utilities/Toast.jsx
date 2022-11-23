3d7a8556d73d05eded5ec920190f268eeb702031
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 03:04:31 +0600

    toast alert updated

--- src/utilities/Toast.jsx
+++ src/utilities/Toast.jsx
@@ -1,35 +1,29 @@
-import React from 'react'
-import Swal from 'sweetalert2';
-import withReactContent from 'sweetalert2-react-content';
+import React from "react";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
 const MySwal = withReactContent(Swal);
 
+function Toast({ type, icon, title },cb) {
+	
+	const Toast = MySwal.mixin({
+		toast: true,
+		position: "top-end",
+		iconColor: "#9ca3af",
+		showConfirmButton: false,
+		timer: 1500,
+		target: ".contentViewer",
+		customClass: {
+			popup: "colored-toast",
+		},
+		timerProgressBar: true,
+	});
 
-
-
-
-function  Toast({type,icon,title}) {
-
-		const Toast = MySwal.mixin({
-		  toast: true,
-		  position: 'top-end',
-		  iconColor: '#9ca3af',
-		  showConfirmButton: false,
-		  timer: 2500,
-		  customClass: {
-			   popup: 'colored-toast'
-			},
-		 timerProgressBar: true
-		})
-
-		Toast.fire({
-		  type: type,
-		  icon : icon,
-		  title: title
-		})
-   
-
-
+	Toast.fire({
+		type: type,
+		icon: icon,
+		title: title,
+	}).then(()=> cb(false))
 }
 
-export {MySwal};
+export { MySwal };
 export default Toast;
