d63c1964b9c23f57dabc9d4e9f35be881fe3f3d1
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-24 02:57:14 +0600

    article card added

---
+++ src/components/dashboard_2/cards/Articles.jsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { Fade } from "react-reveal";
+import Moment from "react-moment";
+import Modal from "../modal/Modal";
+
+function Articles({article}) {
+	const [show, setShow] = useState(false);
+	const [data, setData] = useState({
+		type: "Articles",
+		id: article.id,
+		data: article,
+	});
+	return (
+		<div className="col-md-4 mb-4">
+			{show && <Modal data={data} view={setShow} />}
+			<Fade>
+				<div className="card">
+					<div
+						className="card-body"
+						style={{
+							boxShadow: "0 0.25rem 0.75rem rgb(0 0 0 / 10%)",
+							minHeight: '230px'
+						}}
+					>
+						<h5 className="card-title">
+							{article?.title}
+							<span
+								className={`ml-2 badge ${
+									article.status === "Review"
+										? "badge-warning"
+										: article.status === "Approved"
+										? "badge-success"
+										: "badge-danger"
+								}`}
+							>
+								{article?.status}
+							</span>
+						</h5>
+						<p className="card-text">
+							{article?.description.substr(0, 100)} . . .
+						</p>
+						<div className="d-flex justify-content-between">
+							<p
+								style={{ cursor: "pointer" }}
+								onClick={() => {
+									setShow(true);
+								}}
+							>
+								See Details
+							</p>
+							<p>
+								<Moment fromNow>{article?.date}</Moment>
+							</p>
+						</div>
+					</div>
+				</div>
+			</Fade>
+		</div>
+	);
+}
+
+export default Articles;
