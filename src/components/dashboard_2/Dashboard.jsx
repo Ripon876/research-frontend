9b683157663696b7e174e36f4f079e25d2c669cc
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-26 15:55:41 +0600

    syntext formated

--- src/components/dashboard_2/Dashboard.jsx
+++ src/components/dashboard_2/Dashboard.jsx
@@ -1,18 +1,13 @@
-import {useParams} from 'react-router-dom';
-import M_Dashboard from './moderator/Dashboard';
-import R_Dashboard from './researcher/Dashboard';
+import { useParams } from "react-router-dom";
+import M_Dashboard from "./moderator/Dashboard";
+import R_Dashboard from "./researcher/Dashboard";
 
 function Dashboard() {
-	const {type} = useParams();
+	const { type } = useParams();
+
 	return (
-		<div>
-			{type === 'moderator ' ? 
-				<M_Dashboard />
-				: 
-				<R_Dashboard />
-		    }
-		</div>
-	)
+		<div>{type === "moderator " ? <M_Dashboard /> : <R_Dashboard />}</div>
+	);
 }
 
 export default Dashboard;
