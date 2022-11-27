e59426319daf8d6429e080501218419176d611b3
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-27 17:21:19 +0600

    old dashboard routes removed

--- src/App.js
+++ src/App.js
@@ -10,8 +10,6 @@ import LoginPage from './pages/LoginPage';
 import SignUpPage from './pages/SignUpPage';
 import ResearchPage from './pages/ResearchPage';
 import PublicationPage from './pages/PublicationPage';
-import UserDashboard from './pages/UserDashboard';
-import ModeratorDashboard from './pages/ModeratorDashboard';
 import {InternshipPage,WorkPage} from './pages/ApplyPage';
 import SponsorPage from './pages/SponsorPage';
 import DonateUsPage from './pages/DonateUsPage';
@@ -40,11 +38,6 @@ function App() {
       <Route path='/login' element={<LoginPage />} />
       <Route path='/research' element={<ResearchPage />} />
       <Route path='/publication' element={<PublicationPage />} />
-      {/*<Route path='/dashboard/user/' element={<UserDashboard />} />
-      <Route path='/dashboard/user/:page' element={<UserDashboard />} />
-      <Route path='/dashboard/moderator/' element={<ModeratorDashboard />} />
-      <Route path='/dashboard/moderator/:page' element={<ModeratorDashboard />} />
-      <Route path='/dashboard/moderator/:page/:id' element={<ModeratorDashboard />} />*/}
       <Route path='/apply/internship' element={<InternshipPage />} />
       <Route path='/apply/work' element={<WorkPage />} />
       <Route path='/sponsor_our_program' element={<SponsorPage />} />
