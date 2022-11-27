bb02e6716c246f20fe2466296446f446c46404d9
MD Ripon Islam <islam876ripon@gmail.com>
2022-11-27 17:05:25 +0600

    checking url and setting user in global state

--- src/components/dashboard_2/Dashboard.jsx
+++ src/components/dashboard_2/Dashboard.jsx
@@ -1,12 +1,34 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import M_Dashboard from "./moderator/Dashboard";
 import R_Dashboard from "./researcher/Dashboard";
 
 function Dashboard() {
 	const { type } = useParams();
+	const dispatch = useDispatch();
 
+	useEffect(() => {
+		if (type === "moderator") {
+			dispatch({
+				type: "SET_USER",
+				user: {
+					name: "Mark",
+					type: "Moderator",
+				},
+			});
+		} else {
+			dispatch({
+				type: "SET_USER",
+				user: {
+					name: "Peter",
+					type: "Researcher",
+				},
+			});
+		}
+	}, []);
 	return (
-		<div>{type === "moderator " ? <M_Dashboard /> : <R_Dashboard />}</div>
+		<div>{type === "moderator" ? <M_Dashboard /> : <R_Dashboard />}</div>
 	);
 }
 
