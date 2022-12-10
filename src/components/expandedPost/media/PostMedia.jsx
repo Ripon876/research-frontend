b60457b1f77f078a691c2187fe87f0d5ff67f727
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-10 12:46:47 +0600

    video integreated

--- src/components/expandedPost/media/PostMedia.jsx
+++ src/components/expandedPost/media/PostMedia.jsx
@@ -3,57 +3,42 @@ import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
 import "./PostMedia.css";
 
-function PostMedia({data}) {
+function PostMedia({ data }) {
+	let media = data?.filter((item) => item.type !== "file");
+	console.log(media);
 	return (
 		<div>
-			<OwlCarousel className="owl-theme" 
-			items={1} 
-			loop margin={50}
-			nav={data?.length > 1 ? true : false}
-			dots={false}
-			navContainerClass="owl-nav-container" 
-			navClass={['owl-prev','owl-next']}
+			<OwlCarousel
+				className="owl-theme"
+				items={1}
+				loop
+				margin={50}
+				nav={media?.length > 1 ? true : false}
+				dots={false}
+				navContainerClass="owl-nav-container"
+				navClass={["owl-prev", "owl-next"]}
 			>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/6495ED/000000"
-						alt=""
-					/>
-				</div>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/1E90FF/000000"
-						alt=""
-					/>
-				</div>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/6495ED/000000"
-						alt=""
-					/>
-				</div>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/1E90FF/000000"
-						alt=""
-					/>
-				</div>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/6495ED/000000"
-						alt=""
-					/>
-				</div>
-				<div className="item">
-					<img
-						src="https://via.placeholder.com/350x280/1E90FF/000000"
-						alt=""
-					/>
-				</div>
-
-
+				{media?.map((item) => (
+					<>
+						{item.type === "image" ? (
+							<div className="item">
+								<img src={item?.url} />
+							</div>
+						) : (
+							<div className="item video">
+								<div>
+									<video width="100%" controls>
+										<source
+											src={item?.url}
+											type="video/mp4"
+										/>
+									</video>
+								</div>
+							</div>
+						)}
+					</>
+				))}
 			</OwlCarousel>
-
 		</div>
 	);
 }
