c6f86f19e3362dc97e4aa1fb1bc087d436c500b5
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-12 02:00:40 +0600

    open graph tags added

--- src/components/expandedPost/ExpandedPost.jsx
+++ src/components/expandedPost/ExpandedPost.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
+import { Helmet } from "react-helmet";
 import PostMedia from "./media/PostMedia";
 import PostContent from "./content/PostContent";
 import "./ExpandedPost.css";
@@ -17,27 +18,58 @@ function ExpandedPost() {
 	};
 
 	return (
-		<div style={{ background: '#151515'}}>
-			<div className="container-fluid expandedPostContainer">
-				<div className="row" style={{ background: "rgb(21 21 21)" }}>
+		<>
+			<Helmet>
+				<title>{post[0]?.content.substr(0,50)}. . . </title>
+				<meta
+					property="og:url"
+					content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html"
+				/>
+				<meta property="og:type" content="article" />
+				<meta
+					property="og:title"
+					content="When Great Minds Don’t Think Alike"
+				/>
+				<meta name="description" 	content="How much does culture influence creative thinking?" />
+				<meta
+					property="og:description"
+					content="How much does culture influence creative thinking?"
+				/>
+
+				<meta
+					property="og:image"
+					content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+				/>	
+				<meta
+					property="og:image"
+					content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg"
+				/>
+			</Helmet>
+			<div style={{ background: "#151515" }}>
+				<div className="container-fluid expandedPostContainer">
 					<div
-						className="col-md-8 col-sm-12"
-						style={{ height: "100vh" }}
+						className="row"
+						style={{ background: "rgb(21 21 21)" }}
 					>
 						<div
-							className="closePostButtonContainer"
-							onClick={goBack}
+							className="col-md-8 col-sm-12"
+							style={{ height: "100vh" }}
 						>
-							<i class="icofont-close"></i>
+							<div
+								className="closePostButtonContainer"
+								onClick={goBack}
+							>
+								<i class="icofont-close"></i>
+							</div>
+							<PostMedia data={post[0]?.attachments} />
 						</div>
-						<PostMedia data={post[0]?.attachments} />
-					</div>
-					<div className="col-md-4 col-sm-12 bg-white">
-						<PostContent post={post[0]} />
+						<div className="col-md-4 col-sm-12 bg-white">
+							<PostContent post={post[0]} />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
 
