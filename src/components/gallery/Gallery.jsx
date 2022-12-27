b63153fc8a6d65f6bbfdce28608e5b636c8deb90
MD Ripon Islam <islam876ripon@gmail.com>
2022-12-27 12:59:04 +0600

    lightbox added to about us gallery

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -1,14 +1,14 @@
-import {useEffect} from 'react';
-
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import FsLightbox from "fslightbox-react";
 
 function Gallery() {
-
-
-
-
+	const [lightBox, setLightBox] = useState(false);
+	const [img, setImg] = useState("");
 
 	return (
-		<div className='gallery'>
+		<div className="gallery">
+			<FsLightbox toggler={lightBox} sources={[img]} />
 			<div className="container mt-5 pt-3">
 				<div className="section-title">
 					<h2>Gallery</h2>
@@ -25,12 +25,20 @@ function Gallery() {
 				<div class="row">
 					<div class="col-md-4 col-sm-4 mb-4 mb-lg-0">
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Boat on Calm Water"
 						/>
 
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain1.webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Wintry Mountain Landscape"
@@ -39,12 +47,20 @@ function Gallery() {
 
 					<div class="col-md-4 col-sm-4 mb-4 mb-lg-0">
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain2.webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Mountains in the Clouds"
 						/>
 
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(73).webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Boat on Calm Water"
@@ -53,17 +69,28 @@ function Gallery() {
 
 					<div class="col-md-4 col-sm-4 mb-4 mb-lg-0">
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Horizontal/Nature/4-col/img%20(18).webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Waves at Sea"
 						/>
 
 						<img
+							onClick={(e) => {
+								setImg(e.target.src);
+								setLightBox(!lightBox);
+							}}
 							src="https://mdbcdn.b-cdn.net/img/Photos/Vertical/mountain3.webp"
 							class="w-100 shadow-1-strong rounded mb-4"
 							alt="Yosemite National Park"
 						/>
 					</div>
+				</div>
+				<div className="text-right">
+					<Link to="/gallery">See More</Link>
 				</div>
 			</div>
 		</div>
